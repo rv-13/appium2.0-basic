35b176b0efb375f1c36e48c18a230e97a062996c
rv-13 <r131194@gmail.com>
2023-09-02 15:09:21 +0530

    Initial commit Basic Appium2.0

---
+++ src/test/java/com/test/AndroidTest.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
+import io.appium.java_client.remote.AutomationName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+class AndroidTest {
+
+    @Test
+    void androidTestLaunch() throws MalformedURLException, InterruptedException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");//optional
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);//optional
+        options.setDeviceName("rv-test-device");
+        options.setApp(System.getProperty("user.dir") + "/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        Thread.sleep(4000);
+        driver.findElement(AppiumBy.accessibilityId("open menu")).click();
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(e -> e.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"menu item log in\"]")));
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"menu item log in\"]")).click();
+        Thread.sleep(2000);
+        driver.findElement(AppiumBy.accessibilityId("Username input field")).sendKeys("tesssttt");
+    }
+
+    @Test
+    void iosTestLaunch() throws MalformedURLException, InterruptedException {
+        XCUITestOptions options = new XCUITestOptions();
+        options.setPlatformName("Ios"); //optional
+        options.setAutomationName(AutomationName.IOS_XCUI_TEST);//optional
+        options.setDeviceName("iPhone 14");
+        options.setApp(System.getProperty("user.dir") + "/apps/iOS-Simulator-MyRNDemoApp.1.3.0-162.zip");
+        IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723"), options);
+        Thread.sleep(4000);
+    }
+}
